ae5938ec6efd567f8641327d3e4b9ca77d2a7c03
Kevin Gomes <kevinmg50@gmail.com>
2023-08-11 16:20:49 -0300

    feat: adicionado mais logs ao receber sinal

--- api/client/process.go
+++ api/client/process.go
@@ -7,16 +7,12 @@ import (
 	"syscall"
 )
 
-// HandleShutdown lida com os sinais de desligamento do servidor
 func HandleShutdown() {
-	// Canal para ouvir sinais de desligamento
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
 
-	// Aguardar o sinal de desligamento
 	<-shutdown
 
 	fmt.Println("\nDesligando o servidor...")
-	// TODO: Adicionar lógica de limpeza, se necessário
 	fmt.Println("Servidor desligado com sucesso.")
 }
