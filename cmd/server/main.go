ef39763abdce7b12ec672503117396644dd0b5de
Kevin Gomes <kevinmg50@gmail.com>
2023-08-11 23:36:32 -0300

    feat: load da configuração yaml

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/kevinfinalboss/checklist-apps/api/client"
+	"github.com/kevinfinalboss/checklist-apps/pkg/utils"
 	"github.com/kevinfinalboss/checklist-apps/router"
 )
 
@@ -16,6 +17,11 @@ func main() {
 
 	if err := godotenv.Load(envPath); err != nil {
 		fmt.Println("Erro ao carregar .env")
+	}
+
+	if err := utils.LoadConfig(); err != nil {
+		fmt.Println("Erro ao carregar configurações:", err)
+		return
 	}
 
 	ginMode := os.Getenv("GIN_MODE")
