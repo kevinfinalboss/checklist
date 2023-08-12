6b4d132ff7ec4c39286895ef6a4174938b22a938
Kevin Gomes <kevinmg50@gmail.com>
2023-08-11 23:34:46 -0300

    feat: implementado arquivos de configuração utilizando viper

---
+++ pkg/utils/config.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"github.com/spf13/viper"
+)
+
+func LoadConfig() error {
+	viper.SetConfigName("config")
+	viper.AddConfigPath("../../configs")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+
+	return nil
+}
