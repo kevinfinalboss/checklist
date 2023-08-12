479617faeb20f186ac18098ded368fb23531effd
Kevin Gomes <kevinmg50@gmail.com>
2023-08-11 22:17:34 -0300

    feat: incluido middleware de erro

--- router/router.go
+++ router/router.go
@@ -2,11 +2,13 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/kevinfinalboss/checklist-apps/api/middlewares"
 	"github.com/kevinfinalboss/checklist-apps/pkg/controllers"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+	r.Use(middlewares.ErrorHandler())
 
 	r.GET("/diag/health", controllers.HealthCheck)
 
