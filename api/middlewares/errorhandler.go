01b6f864c7707eead50c411d28616e6a98b282d4
Kevin Gomes <kevinmg50@gmail.com>
2023-08-12 00:19:59 -0300

    feat: adicionado alarmes de erros com notificação via webhook e e-mail

--- api/middlewares/errorhandler.go
+++ api/middlewares/errorhandler.go
@@ -5,13 +5,25 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kevinfinalboss/checklist-apps/pkg/services"
 )
 
 func ErrorHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		defer func() {
 			if err := recover(); err != nil {
-				fmt.Printf("ERROR recovered: %v\n", err)
+				errMsg := fmt.Sprintf("ERROR recovered: %v", err)
+				fmt.Println(errMsg)
+
+				subject := "Erro Interno no Servidor"
+				body := "Ocorreu um erro no servidor: " + errMsg
+				if emailErr := services.SendEmail(subject, body); emailErr != nil {
+					fmt.Println("Erro ao enviar e-mail:", emailErr)
+				}
+
+				if webhookErr := services.SendDiscordWebhook(errMsg); webhookErr != nil {
+					fmt.Println("Erro ao enviar webhook:", webhookErr)
+				}
 
 				c.JSON(http.StatusInternalServerError, gin.H{
 					"status": http.StatusInternalServerError,
