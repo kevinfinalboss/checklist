927243754e292010de19396b9fa58f54461655ae
Kevin Gomes <kevinmg50@gmail.com>
2023-08-12 12:05:15 -0300

    fix: corrigido path para load nos templates

--- router/router.go
+++ router/router.go
@@ -9,7 +9,7 @@ import (
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 	r.Use(middlewares.ErrorHandler())
-	r.LoadHTMLGlob("../../templates/*")
+	r.LoadHTMLGlob("./templates/*")
 
 	r.GET("/diag/health", controllers.HealthCheck)
 	r.GET("/test/panic", func(c *gin.Context) {
