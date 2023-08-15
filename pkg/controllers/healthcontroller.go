ea03c12507c2d4d6d6768c3cf6cbd3a4345c2e1f
Kevin Gomes <kevinmg50@gmail.com>
2023-08-15 13:09:47 -0300

    docs: documentação do endpoint

--- pkg/controllers/healthcontroller.go
+++ pkg/controllers/healthcontroller.go
@@ -4,6 +4,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @Summary Verificar a saúde da API
+// @Description Retorna OK se a API estiver funcionando
+// @Tags Health Check
+// @Produce  json
+// @Success 200 {string} string "OK"
+// @Router /diag/health [get]
 func HealthCheck(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"status": "UP",
