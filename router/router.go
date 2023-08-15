e46adb612445891b7af34f99c15f9ba34f306082
Kevin Gomes <kevinmg50@gmail.com>
2023-08-15 13:07:12 -0300

    feat: implementado swagger para docs

--- router/router.go
+++ router/router.go
@@ -3,7 +3,10 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/kevinfinalboss/checklist-apps/api/middlewares"
+	_ "github.com/kevinfinalboss/checklist-apps/docs"
 	"github.com/kevinfinalboss/checklist-apps/pkg/controllers"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func SetupRouter() *gin.Engine {
@@ -12,6 +15,9 @@ func SetupRouter() *gin.Engine {
 	r.LoadHTMLGlob("./templates/*")
 
 	r.Use(middlewares.RateLimitMiddleware())
+
+	url := ginSwagger.URL("http://localhost:80/swagger/doc.json")
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 
 	r.GET("/diag/health", controllers.HealthCheck)
 	r.GET("/test/panic", func(c *gin.Context) {
