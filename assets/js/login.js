6835c6ab9ac5ab30ebde791b8d0b2e7edd0dce79
Kevin Gomes <kevinmg50@gmail.com>
2023-08-15 21:23:48 -0300

    fix: corrigido função showNotification retornar none

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,5 @@
 const inputs = document.querySelectorAll(".input");
 
-
 function addcl(){
 	let parent = this.parentNode.parentNode;
 	parent.classList.add("focus");
@@ -13,8 +12,33 @@ function remcl(){
 	}
 }
 
-
 inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
+
+window.onload = function() {
+	const urlParams = new URLSearchParams(window.location.search);
+	const sessionExpired = urlParams.get('session_expired');
+	const invalidCredentials = urlParams.get('invalid_credentials');
+	const loginSuccess = urlParams.get('login_success');
+
+	if (sessionExpired === 'true') {
+		showNotification('Sessão Expirada', 'red');
+	} else if (invalidCredentials === 'true') {
+		showNotification('Usuário ou Senha Inválida', 'red');
+	} else if (loginSuccess === 'true') {
+		showNotification('Login Realizado, Redirecionando...', 'green');
+	}
+};
+
+function showNotification(message, color) {
+	const notification = document.getElementById('notification');
+	notification.textContent = message;
+	notification.style.backgroundColor = color;
+	notification.style.animation = 'fadein 3s, fadeout 1s 3s';
+	notification.style.display = 'block';
+	setTimeout(() => {
+		notification.style.display = 'none';
+	}, 4000);
+}
