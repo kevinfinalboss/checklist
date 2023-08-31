e03bc9a1d8176f5109cd52fe668fc9e9545f76ad
Kevin Gomes <kevinmg50@gmail.com>
2023-08-30 22:53:00 -0300

    feat: armazenamento de usuários

---
+++ pkg/repository/user_repository.go
@@ -0,0 +1,29 @@
+package repository
+
+import (
+	"context"
+
+	connection "github.com/kevinfinalboss/checklist-apps/pkg/database"
+	"github.com/kevinfinalboss/checklist-apps/pkg/models"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func CreateUser(user *models.User) error {
+	collection := connection.Client.Database("checklist-apps").Collection("users")
+	_, err := collection.InsertOne(context.Background(), user)
+	return err
+}
+
+func FindUserByCPF(cpf string) (*models.User, error) {
+	var user models.User
+	collection := connection.Client.Database("checklist-apps").Collection("users")
+	err := collection.FindOne(context.Background(), bson.M{"cpf": cpf}).Decode(&user)
+	return &user, err
+}
+
+func FindUserByEmail(email string) (*models.User, error) {
+	var user models.User
+	collection := connection.Client.Database("checklist-apps").Collection("users")
+	err := collection.FindOne(context.Background(), bson.M{"email": email}).Decode(&user)
+	return &user, err
+}
