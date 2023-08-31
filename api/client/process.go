94b5f65f6bd50cda0bc9f4e8740439bdbcc7e10c
Kevin Gomes <kevinmg50@gmail.com>
2023-08-30 22:20:02 -0300

    feat: melhorado liberação de processos ao desligamento

--- api/client/process.go
+++ api/client/process.go
@@ -8,12 +8,21 @@ import (
 	"github.com/kevinfinalboss/checklist-apps/pkg/utils"
 )
 
-func HandleShutdown() {
+type ShutdownHandler interface {
+	ReleaseResources()
+}
+
+func HandleShutdown(handlers ...ShutdownHandler) {
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
 
 	<-shutdown
 
 	utils.Logger.Info("\nDesligando o servidor...")
+
+	for _, handler := range handlers {
+		handler.ReleaseResources()
+	}
+
 	utils.Logger.Info("Servidor desligado com sucesso.")
 }
