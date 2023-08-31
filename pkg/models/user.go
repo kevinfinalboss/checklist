8514aaada74d85dd573066c284ac8d4c5ba77f50
Kevin Gomes <kevinmg50@gmail.com>
2023-08-31 02:25:46 -0300

    feat: adicionado model salt

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -3,13 +3,13 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID                primitive.ObjectID `bson:"_id,omitempty"`
-	Name              string             `bson:"name"`
-	Email             string             `bson:"email"`
-	Password          string             `bson:"password"`
-	ConfirmPassword   string             `bson:"-"`
-	CPF               string             `bson:"cpf"`
-	BirthDate         string             `bson:"birthDate"`
-	Address           string             `bson:"address"`
-	HashedCPFForCheck string             `bson:"hashedCPFForCheck"`
+	ID              primitive.ObjectID `bson:"_id,omitempty"`
+	Name            string             `bson:"name"`
+	Email           string             `bson:"email"`
+	Password        string             `bson:"password"`
+	ConfirmPassword string             `bson:"-"`
+	CPF             string             `bson:"cpf"`
+	BirthDate       string             `bson:"birthDate"`
+	Address         string             `bson:"address"`
+	CPFSalt         string             `bson:"cpfSalt"`
 }
