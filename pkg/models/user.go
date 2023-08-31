4aa7ca8b9f658df49cdf4ac1fa7e03cc8528001a
Kevin Gomes <kevinmg50@gmail.com>
2023-08-31 02:08:15 -0300

    feat: hashecpf added

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -3,12 +3,13 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID              primitive.ObjectID `bson:"_id,omitempty"`
-	Name            string             `bson:"name"`
-	Email           string             `bson:"email"`
-	Password        string             `bson:"password"`
-	ConfirmPassword string             `bson:"-"`
-	CPF             string             `bson:"cpf"`
-	BirthDate       string             `bson:"birthDate"`
-	Address         string             `bson:"address"`
+	ID                primitive.ObjectID `bson:"_id,omitempty"`
+	Name              string             `bson:"name"`
+	Email             string             `bson:"email"`
+	Password          string             `bson:"password"`
+	ConfirmPassword   string             `bson:"-"`
+	CPF               string             `bson:"cpf"`
+	BirthDate         string             `bson:"birthDate"`
+	Address           string             `bson:"address"`
+	HashedCPFForCheck string             `bson:"hashedCPFForCheck"`
 }
