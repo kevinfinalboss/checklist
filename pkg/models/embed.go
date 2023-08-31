94a3c6d0c0f3f4dcc1e63d268829f59882ae9791
Kevin Gomes <kevinmg50@gmail.com>
2023-08-31 15:08:32 -0300

    feat: adicionado novos campos ao embed

--- pkg/models/embed.go
+++ pkg/models/embed.go
@@ -1,12 +1,29 @@
 package models
 
 type Embed struct {
-	Title       string  `json:"title,omitempty"`
-	Description string  `json:"description,omitempty"`
-	Color       int     `json:"color,omitempty"`
-	Footer      *Footer `json:"footer,omitempty"`
+	Title       string       `json:"title,omitempty"`
+	Description string       `json:"description,omitempty"`
+	Color       int          `json:"color,omitempty"`
+	Footer      *Footer      `json:"footer,omitempty"`
+	Image       *Image       `json:"image,omitempty"`
+	Thumbnail   *Thumbnail   `json:"thumbnail,omitempty"`
+	Fields      []EmbedField `json:"fields,omitempty"`
 }
 
 type Footer struct {
 	Text string `json:"text"`
+}
+
+type Image struct {
+	URL string `json:"url"`
+}
+
+type Thumbnail struct {
+	URL string `json:"url"`
+}
+
+type EmbedField struct {
+	Name   string `json:"name"`
+	Value  string `json:"value"`
+	Inline bool   `json:"inline"`
 }
