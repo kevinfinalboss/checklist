8b8fedd6b1e4c5d261a2708a7c2f54e7067661f5
Kevin Gomes <kevinmg50@gmail.com>
2023-08-31 09:34:24 -0300

    feat: aumentado tempo de expiração do token

--- pkg/services/cookie.go
+++ pkg/services/cookie.go
@@ -9,7 +9,7 @@ func SetCookie(w http.ResponseWriter, token string) {
 	http.SetCookie(w, &http.Cookie{
 		Name:     "auth_token",
 		Value:    token,
-		Expires:  time.Now().Add(1 * time.Minute),
+		Expires:  time.Now().Add(1 * time.Hour),
 		HttpOnly: true,
 		Secure:   true,
 		SameSite: http.SameSiteStrictMode,
