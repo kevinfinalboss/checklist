fab9de35e51f6da19fefd4690849673df7fd1a56
Kevin Gomes <kevinmg50@gmail.com>
2023-08-31 09:46:56 -0300

    feat: implementado resgate de usuário por email

--- pkg/controllers/user_controller.go
+++ pkg/controllers/user_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -14,5 +15,25 @@ func GetUserByCPF(c *gin.Context) {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Usuário não encontrado"})
 		return
 	}
+	user.CPF = fmt.Sprintf("%s.%s**.***-**", user.CPF[:3], user.CPF[3:4])
 	c.JSON(http.StatusOK, user)
+}
+
+func GetUserByEmail(c *gin.Context) {
+	email := c.Param("email")
+	user, err := services.GetUserByEmail(email)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Usuário não encontrado"})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func GetUserAll(c *gin.Context) {
+	users, err := services.GetAllUsers()
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Nenhum usuário encontrado"})
+		return
+	}
+	c.JSON(http.StatusOK, users)
 }
