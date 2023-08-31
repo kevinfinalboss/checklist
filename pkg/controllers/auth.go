cb39f35cc096a4fdcbfcc63ac230276ea204abd4
Kevin Gomes <kevinmg50@gmail.com>
2023-08-31 09:37:41 -0300

    feat: melhorado validação de usuários

--- pkg/controllers/auth.go
+++ pkg/controllers/auth.go
@@ -89,13 +89,3 @@ func isValidUser(email, password string) bool {
 
 	return bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)) == nil
 }
-
-func GetUserByCPF(c *gin.Context) {
-	cpf := c.Param("cpf")
-	user, err := services.GetUserByCPF(cpf)
-	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Usuário não encontrado"})
-		return
-	}
-	c.JSON(http.StatusOK, user)
-}
