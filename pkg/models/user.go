db29337fb1fbae219f88cb511a505a06063e63db
Kevin Gomes <kevinmg50@gmail.com>
2023-08-30 22:46:57 -0300

    feat: model do usuário

---
+++ pkg/models/user.go
@@ -0,0 +1,14 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID              primitive.ObjectID `bson:"_id,omitempty"`
+	Name            string             `bson:"name"`
+	Email           string             `bson:"email"`
+	Password        string             `bson:"password"`
+	ConfirmPassword string             `bson:"-"`
+	CPF             string             `bson:"cpf"`
+	BirthDate       string             `bson:"birthDate"`
+	Address         string             `bson:"address"`
+}
