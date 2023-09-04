d688553e483422e80a9b92ca1326b1a1a8e31f6a
Kevin Gomes <kevinmg50@gmail.com>
2023-09-04 14:58:08 -0300

    feat: aumentado proteção a dados sensiveis

---
+++ pkg/services/crypt_service.go
@@ -0,0 +1,40 @@
+package services
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
+)
+
+var secretKey = []byte("SMxP8/GDSrfMjrBxaalq7aIxknlUPwZb8TSFCy7uVZY=")
+
+func Encrypt(text string) (string, error) {
+	block, err := aes.NewCipher(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := make([]byte, len(text))
+	stream := cipher.NewCTR(block, secretKey[:block.BlockSize()])
+	stream.XORKeyStream(ciphertext, []byte(text))
+
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+func Decrypt(encryptedText string) (string, error) {
+	block, err := aes.NewCipher(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	decodedText, err := base64.StdEncoding.DecodeString(encryptedText)
+	if err != nil {
+		return "", err
+	}
+
+	decryptedText := make([]byte, len(decodedText))
+	stream := cipher.NewCTR(block, secretKey[:block.BlockSize()])
+	stream.XORKeyStream(decryptedText, decodedText)
+
+	return string(decryptedText), nil
+}
