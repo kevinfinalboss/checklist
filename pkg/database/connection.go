c35370842cb412d6073f1f5138e0c9df5cc97bd3
Kevin Gomes <kevinmg50@gmail.com>
2023-09-08 22:49:11 -0300

    feat: implementado pool de conexões e only connection ao banco

--- pkg/database/connection.go
+++ pkg/database/connection.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"net"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/kevinfinalboss/checklist-apps/pkg/utils"
@@ -13,50 +14,63 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-var Client *mongo.Client
+var (
+	Client  *mongo.Client
+	once    sync.Once
+	connect sync.Once
+)
 
 func Connect() error {
-	mongoURL := os.Getenv("MONGO_URL")
-	if mongoURL == "" {
-		return errors.New("MONGO_URL não definida no .env")
-	}
-
-	dialer := &net.Dialer{}
-	dialer.Timeout = 10 * time.Second
-
-	clientOptions := options.Client().
-		ApplyURI(mongoURL).
-		SetMaxPoolSize(50).
-		SetDialer(dialer)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
 	var err error
-	for i := 0; i < 3; i++ { 
-		Client, err = mongo.Connect(ctx, clientOptions)
-		if err == nil {
-			break
+
+	once.Do(func() {
+		mongoURL := os.Getenv("MONGO_URL")
+		if mongoURL == "" {
+			err = errors.New("MONGO_URL não definida no .env")
+			return
 		}
-		time.Sleep(2 * time.Second)
-	}
 
-	if err != nil {
-		return errors.New("Erro ao conectar ao MongoDB: " + err.Error())
-	}
+		dialer := &net.Dialer{}
+		dialer.Timeout = 10 * time.Second
 
-	err = Client.Ping(ctx, readpref.Primary())
-	if err != nil {
-		return errors.New("Erro ao fazer ping no MongoDB: " + err.Error())
-	}
+		clientOptions := options.Client().
+			ApplyURI(mongoURL).
+			SetMaxPoolSize(50).
+			SetDialer(dialer)
 
-	utils.Logger.Info("Conectado ao MongoDB com sucesso!")
-	return nil
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		for i := 0; i < 3; i++ {
+			Client, err = mongo.Connect(ctx, clientOptions)
+			if err == nil {
+				break
+			}
+			time.Sleep(2 * time.Second)
+		}
+
+		if err != nil {
+			err = errors.New("Erro ao conectar ao MongoDB: " + err.Error())
+			return
+		}
+
+		err = Client.Ping(ctx, readpref.Primary())
+		if err != nil {
+			err = errors.New("Erro ao fazer ping no MongoDB: " + err.Error())
+			return
+		}
+
+		utils.Logger.Info("Conectado ao MongoDB com sucesso!")
+	})
+
+	return err
 }
 
 func Disconnect() {
-	if Client != nil {
-		Client.Disconnect(context.Background())
-		utils.Logger.Info("Desconectado do MongoDB com sucesso!")
-	}
+	connect.Do(func() {
+		if Client != nil {
+			Client.Disconnect(context.Background())
+			utils.Logger.Info("Desconectado do MongoDB com sucesso!")
+		}
+	})
 }
