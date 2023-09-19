57d5842fe266604d3de63d0c51a684fca13ccbcb
Kevin Gomes <kevinmg50@gmail.com>
2023-09-19 14:32:21 -0300

    refactor: refatorado logica do serviço de emails

--- pkg/services/emailService.go
+++ pkg/services/emailService.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"bytes"
+	"fmt"
 	"html/template"
 	"net/smtp"
 	"os"
@@ -10,14 +11,26 @@ import (
 	"github.com/spf13/viper"
 )
 
-func SendEmail(subject, errorMessage string) error {
-	from := viper.GetString("smtp.from")
-	password := os.Getenv("SMTP_PASSWORD")
-	to := viper.GetString("smtp.to")
-	host := viper.GetString("smtp.host")
-	port := viper.GetString("smtp.port")
+type EmailConfig struct {
+	From     string
+	Password string
+	To       string
+	Host     string
+	Port     string
+}
 
-	templatePath := filepath.Join("templates", "emails", "error.html")
+func LoadEmailConfig() EmailConfig {
+	return EmailConfig{
+		From:     viper.GetString("smtp.from"),
+		Password: os.Getenv("SMTP_PASSWORD"),
+		To:       viper.GetString("smtp.to"),
+		Host:     viper.GetString("smtp.host"),
+		Port:     viper.GetString("smtp.port"),
+	}
+}
+
+func sendEmailWithTemplate(config EmailConfig, subject, message, templateName string) error {
+	templatePath := filepath.Join("templates", "emails", templateName)
 
 	tmpl, err := template.ParseFiles(templatePath)
 	if err != nil {
@@ -26,26 +39,26 @@ func SendEmail(subject, errorMessage string) error {
 
 	var body bytes.Buffer
 	data := struct {
-		ErrorMessage string
+		Message string
 	}{
-		ErrorMessage: errorMessage,
+		Message: message,
 	}
 	if err := tmpl.Execute(&body, data); err != nil {
 		return err
 	}
 
-	msg := "From: " + from + "\n" +
-		"To: " + to + "\n" +
-		"Subject: " + subject + "\n" +
-		"Content-Type: text/html; charset=\"utf-8\"\n\n" +
-		body.String()
+	msg := fmt.Sprintf("From: %s\nTo: %s\nSubject: %s\nContent-Type: text/html; charset=\"utf-8\"\n\n%s",
+		config.From, config.To, subject, body.String())
 
-	auth := smtp.PlainAuth("", from, password, host)
+	auth := smtp.PlainAuth("", config.From, config.Password, config.Host)
 
-	err = smtp.SendMail(host+":"+port, auth, from, []string{to}, []byte(msg))
-	if err != nil {
-		return err
-	}
+	return smtp.SendMail(config.Host+":"+config.Port, auth, config.From, []string{config.To}, []byte(msg))
+}
 
-	return nil
+func SendErrorNotification(config EmailConfig, subject, errorMessage string) error {
+	return sendEmailWithTemplate(config, subject, errorMessage, "error.html")
+}
+
+func SendLoginNotification(config EmailConfig, subject, loginMessage string) error {
+	return sendEmailWithTemplate(config, subject, loginMessage, "login_notify.html")
 }
