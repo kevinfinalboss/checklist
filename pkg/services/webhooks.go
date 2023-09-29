4a08513b7b08a2892641a945eb42abd0bb74f845
Kevin Gomes <kevinmg50@gmail.com>
2023-09-29 16:36:29 -0300

    feat: estilizado embed

--- pkg/services/webhooks.go
+++ pkg/services/webhooks.go
@@ -19,26 +19,32 @@ type DiscordWebhook struct {
 func SendDiscordWebhook(title, description string, fields []models.EmbedField) error {
 	webhookURL := viper.GetString("webhooks.discord")
 	if webhookURL == "" {
-		err := fmt.Errorf("Webhook URL não configurada")
-		fmt.Println("Erro no SendDiscordWebhook:", err)
-		return err
+		return fmt.Errorf("Webhook URL não configurada")
 	}
 
 	embed := models.Embed{
 		Title:       title,
 		Description: description,
-		Fields:      fields,
+		Color:       16711680,
+		Footer: &models.Footer{
+			Text: "AuthAPI - Notifier",
+		},
+		Image: &models.Image{
+			URL: "https://github.com/kevinfinalboss/StoreOps/blob/master/screenshots/Logo.jpg?raw=true",
+		},
+		Thumbnail: &models.Thumbnail{
+			URL: "https://github.com/kevinfinalboss/StoreOps/blob/master/screenshots/Logo.jpg?raw=true",
+		},
+		Fields: fields,
 	}
 
 	jsonPayload, err := json.Marshal(DiscordWebhook{Embeds: []models.Embed{embed}})
 	if err != nil {
-		fmt.Println("Erro ao serializar o payload do webhook:", err)
 		return err
 	}
 
 	req, err := http.NewRequest("POST", webhookURL, bytes.NewBuffer(jsonPayload))
 	if err != nil {
-		fmt.Println("Erro ao criar nova requisição HTTP:", err)
 		return err
 	}
 
@@ -46,7 +52,6 @@ func SendDiscordWebhook(title, description string, fields []models.EmbedField) e
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Erro ao enviar a requisição do webhook:", err)
 		return err
 	}
 
@@ -55,13 +60,8 @@ func SendDiscordWebhook(title, description string, fields []models.EmbedField) e
 		resp.Body.Close()
 	}()
 
-	respBody, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("Corpo da Resposta:", string(respBody))
-
 	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusNoContent {
-		err := fmt.Errorf("Resposta de erro do Discord: %s", resp.Status)
-		fmt.Println("Erro no SendDiscordWebhook:", err)
-		return err
+		return fmt.Errorf("Resposta de erro do Discord: %s", resp.Status)
 	}
 
 	return nil
