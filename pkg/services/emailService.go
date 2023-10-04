c9d65c9117e8bf8782e3a21ac0e5071a2e519c51
Kevin Gomes <kevinmg50@gmail.com>
2023-10-03 22:18:31 -0300

    feat: adicionado notificiação de criação de conta

--- pkg/services/emailService.go
+++ pkg/services/emailService.go
@@ -7,6 +7,7 @@ import (
 	"net/smtp"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/spf13/viper"
 )
@@ -70,4 +71,47 @@ func SendErrorNotification(config EmailConfig, subject, errorMessage string) err
 
 func SendLoginNotification(config EmailConfig, recipientEmail, subject, loginMessage string) error {
 	return sendEmailWithTemplate(config, recipientEmail, subject, loginMessage, "login_notify.html")
+}
+
+func SendAccountCreationNotification(config EmailConfig, recipientEmail, userName string) error {
+	templateName := "account_creation_notification.html"
+
+	currentTime := time.Now()
+	formattedTime := currentTime.Format("02/01/2006 às 15:04")
+
+	messageData := struct {
+		UserName    string
+		CreatedTime string
+	}{
+		UserName:    userName,
+		CreatedTime: formattedTime,
+	}
+
+	return sendEmailWithTemplateWithData(config, recipientEmail, "Conta Criada com Sucesso", templateName, messageData)
+}
+
+func sendEmailWithTemplateWithData(config EmailConfig, recipientEmail, subject, templateName string, data interface{}) error {
+	templatePath := filepath.Join("templates", "emails", templateName)
+
+	tmpl, err := template.ParseFiles(templatePath)
+	if err != nil {
+		return err
+	}
+
+	var body bytes.Buffer
+	if err := tmpl.Execute(&body, data); err != nil {
+		return err
+	}
+
+	msg := fmt.Sprintf("From: %s\nTo: %s\nSubject: %s\nContent-Type: text/html; charset=\"utf-8\"\n\n%s",
+		config.From, recipientEmail, subject, body.String())
+
+	auth := smtp.PlainAuth("", config.From, config.Password, config.Host)
+
+	err = smtp.SendMail(config.Host+":"+config.Port, auth, config.From, []string{recipientEmail}, []byte(msg))
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
